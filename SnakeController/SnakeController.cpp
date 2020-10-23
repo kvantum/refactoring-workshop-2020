bac6673886d9404400c585510414f7fe0b5af024
kvantum <kvm_corp@ukr.net>
2020-10-23 17:31:57 +0200

    Dynamic cast replace

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -216,16 +216,29 @@ Controller::Segment Controller::getNewHead() const
 void Controller::receive(std::unique_ptr<Event> e)
 {
     try {
-        handleTimePassed(*dynamic_cast<EventT<TimeoutInd> const&>(*e));
+        if (e->getMessageId() == 0x20)
+        {
+          handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
+        }
     } catch (std::bad_cast&) {
         try {
-            handleDirectionChange(*dynamic_cast<EventT<DirectionInd> const&>(*e));
-        } catch (std::bad_cast&) {
+              if (e->getMessageId() == 0x10)
+              {
+                handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
+              }
+      } catch (std::bad_cast&) {
+
             try {
-                handleFoodPositionChange(*dynamic_cast<EventT<FoodInd> const&>(*e));
+                if (e->getMessageId() == 0x40)
+                {
+                    handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
+                }
             } catch (std::bad_cast&) {
                 try {
-                    handleNewFood(*dynamic_cast<EventT<FoodResp> const&>(*e));
+                  if (e->getMessageId() == 0x42)
+                  {
+                    handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+                  }
                 } catch (std::bad_cast&) {
                     throw UnexpectedEventException();
                 }
